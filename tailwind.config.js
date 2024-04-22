a90ffb4f3385fc7c91fb480edf4379b654fcc76d
Harmansingh <harmans6282@gmail.com>
2024-04-22 16:39:17 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {},
+    // colors: {
+    //   mainColor: '#D3D9D4',
+    //   TextBlack: '#1D1D1D'
+    // },
+
+    fontFamily:{
+      'Nunito': ["Nunito", "sans-serif"]
+    }
+
+  },
+  plugins: [],
+}
+
